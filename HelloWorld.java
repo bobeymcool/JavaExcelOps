626c5ca27c628c64791bbb3bc0778940ff82a285
bobeymcool <33654193+bobeymcool@users.noreply.github.com>
2020-10-29 18:21:42 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
+public static void main(String args[]){
+    
+    class HelloWorld {
+            public static void main(String[] args) {
+                System.out.println("Hello, World!"); 
+            }
+        }
 }
